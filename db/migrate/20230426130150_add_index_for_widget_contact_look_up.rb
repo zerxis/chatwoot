b8f98fa5adc54ba7d8738ab55e85f2f95fc4c834
Pranav Raj S <pranav@chatwoot.com>
2023-05-11 12:19:53 -0700

    If not exists

--- db/migrate/20230426130150_add_index_for_widget_contact_look_up.rb
+++ db/migrate/20230426130150_add_index_for_widget_contact_look_up.rb
@@ -2,6 +2,6 @@ class AddIndexForWidgetContactLookUp < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    add_index :contacts, 'LOWER(email), account_id', name: 'index_contacts_on_lower_email_account_id', algorithm: :concurrently
+    add_index :contacts, 'LOWER(email), account_id', name: 'index_contacts_on_lower_email_account_id', algorithm: :concurrently, if_not_exists: true
   end
 end
