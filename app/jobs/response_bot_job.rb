2ed88b0c37fcf006943713e219ed4d91c5f828d3
Sojan <sojan@pepalo.com>
2023-07-21 19:17:16 +0300

    chore: clean up

--- app/jobs/response_bot_job.rb
+++
@@ -1,7 +0,0 @@
-class ResponseBotJob < ApplicationJob
-  queue_as :medium
-
-  def perform(conversation)
-    MessageTemplates::Template::ResponseBotService.new(conversation: conversation).perform
-  end
-end
