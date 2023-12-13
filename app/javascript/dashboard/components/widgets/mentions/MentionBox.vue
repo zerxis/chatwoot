6fa3a37a8cab50209a7bc1c30eae671cb490a9ce
iamsivin <iamsivin@gmail.com>
2023-12-13 17:36:52 +0530

    chore: Fix overflow

--- app/javascript/dashboard/components/widgets/mentions/MentionBox.vue
+++ app/javascript/dashboard/components/widgets/mentions/MentionBox.vue
@@ -13,14 +13,14 @@
         @mouseover="onHover(index)"
       >
         <button
-          class="flex group flex-col gap-0.5 cursor-pointer items-start py-4 px-3.5 justify-center w-full h-full text-left hover:bg-woot-25 dark:hover:bg-woot-800 border-b border-solid border-slate-100 dark:border-slate-700"
+          class="flex group flex-col gap-0.5 overflow-hidden cursor-pointer items-start py-4 px-3.5 justify-center w-full h-full text-left hover:bg-woot-25 dark:hover:bg-woot-800 border-b border-solid border-slate-100 dark:border-slate-700"
           :class="{
             ' bg-woot-25 dark:bg-woot-800': index === selectedIndex,
           }"
           @click="onListItemSelection(index)"
         >
           <p
-            class="text-slate-900 dark:text-slate-100 group-hover:text-woot-500 dark:group-hover:text-woot-500 font-medium mb-0 text-sm"
+            class="text-slate-900 dark:text-slate-100 group-hover:text-woot-500 dark:group-hover:text-woot-500 font-medium mb-0 text-sm overflow-hidden text-ellipsis whitespace-nowrap min-w-0 max-w-full"
             :class="{
               'text-woot-500 dark:text-woot-500': index === selectedIndex,
             }"
@@ -28,7 +28,7 @@
             {{ item.label }}
           </p>
           <p
-            class="text-slate-700 dark:text-slate-300 group-hover:text-woot-500 dark:group-hover:text-woot-500 mb-0 text-xs"
+            class="text-slate-700 dark:text-slate-300 group-hover:text-woot-500 dark:group-hover:text-woot-500 mb-0 text-xs overflow-hidden text-ellipsis whitespace-nowrap min-w-0 max-w-full"
             :class="{
               'text-woot-500 dark:text-woot-500': index === selectedIndex,
             }"
